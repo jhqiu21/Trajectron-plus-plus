11c8c5dcaa511ec56d90f9e56897b1055a3a8847
ZackAXue <jhqiu06@outlook.com>
2026-01-01 21:52:54 +0800

    update path

--- trajectron/argument_parser.py
+++ trajectron/argument_parser.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--conf",
                     help="path to json config file for hyperparameters",
                     type=str,
-                    default='../config/config.json')
+                    default='config/config.json')
 
 parser.add_argument("--debug",
                     help="disable all disk writing processes.",
@@ -97,7 +97,7 @@ parser.add_argument('--no_edge_encoding',
 parser.add_argument("--data_dir",
                     help="what dir to look in for data",
                     type=str,
-                    default='../experiments/processed')
+                    default='/c20250502/mid/processed_data')
 
 parser.add_argument("--train_data_dict",
                     help="what file to load for training data",
@@ -112,7 +112,7 @@ parser.add_argument("--eval_data_dict",
 parser.add_argument("--log_dir",
                     help="what dir to save training information (i.e., saved models, logs, etc)",
                     type=str,
-                    default='../experiments/logs')
+                    default='/c20250502/mid/traj')
 
 parser.add_argument("--log_tag",
                     help="tag for the log folder",
